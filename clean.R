d406305ccda2cff1c669fb267ab65f3ed0211fe4
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-10-04 13:03:01 -0400

    Saving clean.R

--- clean.R
+++ clean.R
@@ -2,11 +2,12 @@
 clean_data <- raw_data
 
 #re-use the dat from before
-readStates<- function(states)
+readStates <- function(states)
 {
     #remove rows that are not needed
     #first row is the total for the US, we do not need that
-    states<- states[-1,]
+    states <- states[-1,]
+    
     #last row is Puerto Rico, it's not a state
     num.row <- nrow(states)
     states <- states[-num.row,]
